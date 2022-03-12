5ed3ccb53b2dfcf0bf8d98f7b8b1133fec09ee99
Krzysjwisniewski <krzysjwisniewski@gmail.com>
2022-03-12 09:41:19 +0100

    rwd

--- scrollowanie/scrollowanie.js
+++ scrollowanie/scrollowanie.js
@@ -4,17 +4,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
     topBtn.addEventListener("click", ()=>{
         //window.scrollTo(0, 0); // przeskok do pozycji(0,0)
         window.scrollTo({top: 0, behavior: 'smooth'});
-    });
-    
+    })
+
 });
 
 document.addEventListener("scroll", ()=>{
     //console.log("Przewijanie: " + window.scrollY);
-    let topBtn = document.querySelector("#top");
     if(window.scrollY > 300){
         document.querySelector("#top").classList.add("show");
+        document.querySelector("header").classList.add("scroll");
     }
     else{
         document.querySelector("#top").classList.remove("show");
+        document.querySelector("header").classList.remove("scroll");
     }
+    
 });
