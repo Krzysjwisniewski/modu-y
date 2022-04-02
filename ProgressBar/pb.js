c6dbbe40b24b6d1226f4ef2c163bea9f53c29abe
Krzysjwisniewski <krzysjwisniewski@gmail.com>
2022-04-02 09:33:11 +0200

    galeria

--- ProgressBar/pb.js
+++ ProgressBar/pb.js
@@ -1,9 +1,24 @@
-let div = document.querySelector("div");
-document.addEventListener("scroll", ()=>{
-    let przewijanie = window.scrollY;
-    console.log("Przewijanie: " + przewijanie);
-    let tekst = document.querySelector("main");
-    let wysokość = tekst.offsetHeight;
-    console.log("Wysokość: "+ wysokość);
-    div.style.width = przewijanie*1/wysokość
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    let contentHeight = document.querySelector("main").offsetHeight;
+    console.log("Wysokość dokumentu: " + contentHeight);
+
+    let pb = document.querySelector("#progressBar > div");
+
+    let windowHeight = window.outerHeight;
+    console.log("Wysokość okna: " + windowHeight)
+
+    document.addEventListener("scroll", ()=>{
+
+        let scrollSize = window.scrollY;
+        console.log("Przewinięte: " + scrollSize);
+
+        let procent = (scrollSize/(contentHeight-windowHeight))*100;
+        console.log("Procent: " + procent + "%");
+
+        pb.style.width = procent + "%";
+        // obliczyć jaki % został przewinięty
+        // i taki ustawić wew łaściwościach paska postępu
+    });
+
 });
